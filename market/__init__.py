efe6b657e86368c39f56e519a4a1550c41ac3e37
Thyago Manhães <thyago.ufs@gmail.com>
2021-07-16 00:31:18 -0300

    Login funcionando e várias melhorias no projeto

--- market/__init__.py
+++ market/__init__.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_manager
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '437203c1a8fc68d4df93a567'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app) # starting generating hash 
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from market import routes
